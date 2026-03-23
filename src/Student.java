c543528467ad31bb15e6c6fcb77990c4d0989da1
abdirasakidris638-ship-it <abdirasakidris638@gmail.com>
2026-03-22 21:05:52 -1100

    mai-class-to-test-students-objects
    added main class to test students classes

--- src/Student.java
+++ src/Student.java
@@ -8,3 +8,23 @@ public class Student {
         this.name = name;
         this.studentId = studentId;
     }
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for studentId
+    public int getStudentId() {
+        return studentId;
+    }
+
+    // Setter for studentId
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+}
